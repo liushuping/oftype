c602eec3a9fea05fc514019faf3382a150e8a556
Shuping LIU <lsp.lhh@gmail.com>
2014-08-05 16:46:05 +0000

    initial commit

---
+++ oftype.js
@@ -0,0 +1,23 @@
+function oftype(val, type) {
+    if (type === undefined ||
+        type === null ||
+	val === undefined ||
+	val === null) {
+	return type === val;
+
+    } else if (type === Number) {
+	return typeof val === 'number';
+
+    } else if (type === String) {
+	return typeof val === 'string';
+
+    } else if (type === Boolean) {
+	return typeof val === 'boolean';
+
+    } else {
+	return type instanceof Function &&
+	       val instanceof type;
+    }
+}
+
+module.exports = oftype;
